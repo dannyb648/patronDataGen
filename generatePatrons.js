fc2f05ccad46ed083ab18af3168e24c9e3248e80
Dan Beglin <dbeglinuk@gmail.com>
2018-03-10 16:49:23 +0000

    added frame

---
+++ generatePatrons.js
@@ -0,0 +1,6 @@
+var patrons = []
+for(i = 0; i < 100; i++){
+	var age = Math.floor(Math.random() * (97 - 18) ) + 18;
+	var person = {'_id': i.toString(), 'x':, 'y':, 'state':{'age': age, 'drinkLevel': 0, }}
+	patrons.push()
+}
