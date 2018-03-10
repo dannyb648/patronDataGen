57e366320dd3d6553816665361f68aa440d032cb
Dan Beglin <dbeglinuk@gmail.com>
2018-03-10 20:10:05 +0000

    added pace/age relationship

--- generatePatrons.js
+++ generatePatrons.js
@@ -1,23 +1,33 @@
 const basePeople = require('./basePeople.js')
 
-var patrons = []
-for(i = 0; i < 100; i++){
-	var age = Math.floor(Math.random() * (97 - 18) ) + 18;
-	var pace = Math.round((Math.random() + 1) * 10) / 10;
-	var person = {
-					'_id': i.toString(),
-					'name': basePeople[i].name,
-					'gender': basePeople[i].gender,
-					'beverage': basePeople[i].beverage,
-					'x': 10,
-					'y': 200,
-					'state':{
-						'age': age, 
-						'drinkLevel': 0, 
-						'pace': pace
-					}
-				};
-	patrons.push(person)
+var calcPace = function(age){
+	return 2 - (Math.round((age / 100) * 10) / 10)
 }
 
-console.log(patrons)
+var genPeople = function(amount){
+	var patrons = []
+	for(i = amount; i < 100; i++){
+		var age = Math.floor(Math.random() * (100 - 18) ) + 18;
+		var person = {
+						'_id': i.toString(),
+						'name': basePeople[i].name,
+						'gender': basePeople[i].gender,
+						'age': age, 
+						'beverage': basePeople[i].beverage,
+						'x': 10,
+						'y': 200,
+						'state':
+						{
+							'drinkLevel': 0, 
+							'pace': calcPace(age),
+							'leaving': false
+						}
+					};
+		patrons.push(person)
+	}
+	return patrons;
+};
+
+var x = genPeople(10)
+
+console.log(x)
