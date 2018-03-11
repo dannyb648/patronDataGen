ad686100e3c4320c02a23dd73eb609a1d3aef806
Dan Beglin <dbeglinuk@gmail.com>
2018-03-11 22:23:32 +0000

    added seat logic

--- generatePatrons.js
+++ generatePatrons.js
@@ -20,7 +20,9 @@ var genPeople = function(amount){
 						{
 							'drinkLevel': 0, 
 							'pace': calcPace(age),
-							'leaving': false
+							'leaving': false,
+							'seat': null,
+
 						}
 					};
 		patrons.push(person)
@@ -28,6 +30,72 @@ var genPeople = function(amount){
 	return patrons;
 };
 
-var x = genPeople(10)
+var calcCapacity = function(seats){
+	var sum = 0;
+	for (var key in seats) {
+	    if (seats.hasOwnProperty(key)) {
+	        sum = sum + seats[key].seated.length;
+	    }
+	}
+	return sum
+}
 
-console.log(x)
+var calcMaxCapacity = function(seats){
+	var sum = 0;
+	for (var key in seats) {
+	    if (seats.hasOwnProperty(key)) {
+	        sum = sum + seats[key].max;
+	    }
+	}
+	return sum
+}
+
+var patrons = genPeople(10)
+
+var times = {
+				'openingTime': 1520676000, 
+				'closingTime': 1520730000,
+	 			'timePeriod': this.closingTime - this.openingTime,
+				'interval': (60 * 5)
+			}
+
+
+
+var seats = {
+				'table1': {'seated': [1,2,3,4], 'max': 4},
+				'table2': {'seated': [], 'max': 4},
+				'table3': {'seated': [], 'max': 4},
+				'table4': {'seated': [], 'max': 4},
+				'table5': {'seated': [], 'max': 4},
+				'booth1': {'seated': [], 'max': 8},
+				'booth2': {'seated': [], 'max': 8},
+				'booth3': {'seated': [], 'max': 4},
+				'bar': {'seated': [], 'max': 8},
+				'bench': {'seated': [], 'max': 20}
+			} 
+
+var gen = function(patrons, seats, times){
+	for(i = times.openingTime; i < times.closingTime; i = i + times.interval){
+		var max = calcMaxCapacity(seats)
+		var cap = calcCapacity(seats)
+		if(max - cap == 0){
+			console.log("0%")
+		} else {
+			if((max - cap) < (max / 4)){
+				console.log("remaining cap 1-25%")
+			} else if((max - cap) < (max / 2)){
+				console.log("Remining cap 26-50%")
+			} else if((max - cap) < ((max / 4)* 3)){
+				console.log("Remaining cap 51-75%")
+			} else {
+				console.log("Remaining cap 76-100%")
+			}
+		}
+		console.log(i)
+
+	}
+}
+
+
+var patrons = genPeople(100)
+console.log(gen(patrons, seats, times))
